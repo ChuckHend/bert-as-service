41f5e13bd849a26071d054fda5cc5a3d18cba533
Adam Hendel <hendel.adam@gmail.com>
2019-09-19 19:58:36 -0500

    use large model

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-bert-serving-start  -model_dir ./model/cased_L-12_H-768_A-12
+model_small=cased_L-12_H-768_A-12
+model_large=cased_L-24_H-1024_A-16
+bert-serving-start  -model_dir ./model/${model_large}
--- model/get_model.sh
+++ model/get_model.sh
@@ -1,5 +1,10 @@
-URL=https://storage.googleapis.com/bert_models/2018_10_18/cased_L-12_H-768_A-12.zip
+model_small=cased_L-12_H-768_A-12.zip
+model_large=cased_L-24_H-1024_A-16.zip
+
+model=${model_large}
+
+URL=https://storage.googleapis.com/bert_models/2018_10_18/${model}
 
 wget $URL
 
-unzip -o cased_L-12_H-768_A-12.zip
+unzip -o ${model}
