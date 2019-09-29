ede279f3bac416622daa6891a4be3f3f3cf48b16
Adam Hendel <hendel.adam@gmail.com>
2019-09-29 17:02:19 -0500

    add comments

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 model_small=cased_L-12_H-768_A-12
 model_large=cased_L-24_H-1024_A-16
-bert-serving-start  -model_dir ./model/${model_large}
+bert-serving-start  -model_dir ./model/${model_small}
+# also change model in ./models/get_model.sh
--- model/get_model.sh
+++ model/get_model.sh
@@ -1,8 +1,8 @@
 model_small=cased_L-12_H-768_A-12.zip
 model_large=cased_L-24_H-1024_A-16.zip
 
-model=${model_large}
-
+model=${model_small}
+# also edit model in 
 URL=https://storage.googleapis.com/bert_models/2018_10_18/${model}
 
 wget $URL
